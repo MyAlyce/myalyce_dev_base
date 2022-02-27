368dd753ea6f9982d633526c742e4fed5d936e28
Bubba Skrimp <brewster.joshua1@gmail.com>
2022-02-27 01:04:25 -0800

    Update app.tsx

--- app/src/app.tsx
+++ app/src/app.tsx
@@ -9,7 +9,6 @@ import './init'
 // We need to inject any CSS we want to use, any scss will be converted
 let cssloader = [
   'src/react/styles/index.scss',
-  'src/react/styles/spectre.css/',
   'src/react/styles/spectre.css/dist/spectre-exp.min.css',
   'node_modules/my-alyce-component-lib/dist/index.css'
 ];
